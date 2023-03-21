258410bfa58e0feb8aa73c400715fdc4c3e6c076
Will Train <willtrain@virginmedia.com>
2023-03-21 21:39:50 +0000

    Add project files.

---
+++ ConsoleHelpers/ConsoleApp.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleHelpers
+{
+    public static class ConsoleApp
+    {
+        public static string? GetInput(Func<string?, bool> predicate, string failureMessage)
+        {
+            var input = Console.ReadLine();
+            while (predicate(input))
+            {
+                Console.WriteLine(failureMessage);
+                input = Console.ReadLine();
+            }
+            return input;
+        }
+
+        public static void LoopProgram(
+            Action action,
+            string repeatMessage = "To run again, press 'y', to stop press 'n'")
+        {
+            while (true)
+            {
+                action();
+
+                Console.WriteLine();
+                Console.WriteLine(repeatMessage);
+
+                var input = Console.ReadLine();
+                var lower = input?.ToLower();
+                if (lower != "y")
+                {
+                    Console.WriteLine("Exiting program...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
