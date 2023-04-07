48c3130f7aea0b3c41258b12e9bdade29aba3449
Will Train <willtrain@virginmedia.com>
2023-04-07 11:55:24 +0100

    Adding Generic Menu that returns generic type

---
+++ ConsoleHelpers/Menu/MenuT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelpers
+{
+    internal class Menu<T> : MenuItem<T>
+    {
+        public Menu(params IMenuItem<T>[] menuItems) : this("Menu", null, menuItems)
+        { }
+
+        public Menu(string title, params IMenuItem<T>[] menuItems) : this(title, null, menuItems)
+        { }
+
+        public Menu(string title, string? description, params IMenuItem<T>[] menuItems)
+            : base(title, description, menuItems)
+        {
+            foreach (var item in menuItems)
+            {
+                item.MainMenu = this;
+            }
+        }
+
+        public T Show(MenuSettings settings)
+        {
+            return Display(settings, Array.Empty<string>());
+        }
+
+        protected override void ShowMainMenuOptionIfTrue(bool _, StringBuilder __, MenuSettings ___)
+        {
+            return;
+        }
+    }
+}
